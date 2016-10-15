ab8456cd286b49c7d4484687a51bc386807d6c45
Andrii Halabuda <losAngelesBoy11@gmail.com>
2016-10-15 21:46:03 +0200

    Added new VC, added functionality for Search Bar

---
+++ Pokedex-App/PokemonDetailVC.swift
@@ -0,0 +1,22 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex-App
+//
+//  Created by Andrew Foster on 10/15/16.
+//  Copyright © 2016 fosterLabs. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
